8f5bf1cd2ba427851ced8a66bc467e5549925cb2
leitolml1 <leonelhd4@gmail.com>
2024-09-19 18:45:37 -0300

    Arreglando cagadas de esteban

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -19,7 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("videojuegos/",include("videojuegos.urls")),
     path("alquiler/",include("alquiler.urls")),
 
 ]
