b8feb83878223d7b9d94a8e840c9cb86d49db6cf
Esteban <ardayaesteban@gmail.com>
2024-09-19 18:09:48 -0300

    v y p creado

--- plataforma/models.py
+++ plataforma/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+id = models.IntegerField(primary_key=True)
+nombre = models.CharField(max_length=200)
--- videojuegos/models.py
+++ videojuegos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from plataforma.models import plataforma
+from genero.models import genero
 
 # Create your models here.
+class videojuego(models.Model):
+    id = models.IntegerField()
+    titulo = models.CharField(max_length=200)
+    plataforma = models.ForeignKey('plataformma',on_delete=models.CASCADE)
+    genero = models.ForeignKey('genero',on_delete=models.CASCADE)
+    stock = models. IntegerField() #ver esto
+    
