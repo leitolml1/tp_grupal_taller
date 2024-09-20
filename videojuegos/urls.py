ca3f41678b94cca6f56712dd9433f3249ee26c37
leitolml1 <leonelhd4@gmail.com>
2024-09-20 19:03:04 -0300

    funciones para filtrar genero y plataforma agregado

--- videojuegos/urls.py
+++ videojuegos/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.videojuego_lista, name="videojuego"),
     path('agregar_videojuego/', views.agregar_videojuego, name='agregar_videojuego'),
+    path('filtrar_genero',views.filtrar_genero,name="filtrar_genero"),
+    path('filtrar_plataforma',views.filtrar_plataforma,name="filtrar_plataforma"),
+
 ]
--- videojuegos/views.py
+++ videojuegos/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, redirect
 from .models import videojuego
 from .forms import VideojuegoForm
+from genero.models import genero
+from plataforma.models import plataforma
 
 # Create your views here.
 
 def videojuego_lista(request):
-    juegos = videojuego.objects.all()
-    return render(request, 'videojuegos.html', {'juegos': juegos})
-
+    generos = genero.objects.all()  # Obtén todos los géneros
+    juegos = videojuego.objects.all()  # Obtén todos los videojuegos por defecto
+    plataformas=plataforma.objects.all()
+    return render(request,"videojuegos.html",{
+        "generos":generos,
+        "juegos":juegos,
+        "plataformas":plataformas,
+    })
 def agregar_videojuego(request):
     if request.method == 'POST':
         form = VideojuegoForm(request.POST)
@@ -19,3 +26,37 @@ def agregar_videojuego(request):
             "form":VideojuegoForm()
         })
 
+def filtrar_genero(request):
+    generos = genero.objects.all()  # Obtén todos los géneros
+    juegos = videojuego.objects.all()  # Obtén todos los videojuegos por defecto
+    plataformas = plataforma.objects.all()  # Obtén todos los géneros
+
+    if request.method == "POST":
+        genero_id = request.POST.get('genero')  # Obtén el ID del género seleccionado
+        if genero_id and genero_id != "noFiltrar":
+        
+            juegos=videojuego.objects.filter(genero_id=genero_id)
+
+    context = {
+        'plataformas': plataformas,
+        'juegos': juegos,
+        'generos': generos,
+    }
+    return render(request, 'videojuegos.html', context)
+def filtrar_plataforma(request):
+    generos = genero.objects.all()  # Obtén todos los géneros
+    plataformas = plataforma.objects.all()  # Obtén todos los géneros
+    juegos = videojuego.objects.all()  # Obtén todos los videojuegos por defecto
+
+    if request.method == "POST":
+        plataformas_id = request.POST.get('plataforma')  # Obtén el ID del género seleccionado
+        if plataformas_id and plataformas_id != "noFiltrar":
+        
+            juegos=videojuego.objects.filter(plataforma_id=plataformas_id)
+
+    context = {
+        'juegos': juegos,
+        'generos': generos,
+        'plataformas': plataformas,
+    }
+    return render(request, 'videojuegos.html', context)
